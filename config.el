6a32bf0d6480c8653501b34d3ca9eeaeff61786d
Colin Gabrielson <colin.gabrielson@gmail.com>
2024-04-03 22:01:04 -0400

    They also surf who only stand on waves.

--- config.el
+++ config.el
@@ -5,12 +5,44 @@
 ;;
 (setq user-full-name "Colin Gabrielson"
       user-mail-address "colin.gabrielson@gmail.com"
-      ispell-dictionary "english")
+      doom-scratch-initial-major-mode 'lisp-interaction-mode
+      doom-theme 'doom-one
+      display-line-numbers-type nil
+      load-prefer-newer t
+      +zen-text-scale 1
+      writeroom-extra-line-spacing 0.3
 
-;; Change theme based on if in terminal or gui
-(if (display-graphic-p)
-    (setq doom-theme 'doom-gruvbox)
-  (setq doom-theme 'doom-gruvbox))
+      lsp-ui-sideline-enable nil
+      lsp-enable-symbol-highlighting nil
+      search-highlight t
+      search-whitespace-regexp ".*?"
+      org-directory "~/org/"
+      org-ellipsis " ▼ "
+      org-adapt-indentation nil
+      org-habit-show-habits-only-for-today t)
+
+(use-package! dired-narrow
+  :commands (dired-narrow-fuzzy)
+  :init
+  (map! :map dired-mode-map
+        :desc "narrow" "/" #'dired-narrow-fuzzy))
+
+;; Consider these
+;;(use-package! smartparens
+;;  :init
+;;  (map! :map smartparens-mode-map
+;;        "C-M-f" #'sp-forward-sexp
+;;        "C-M-b" #'sp-backward-sexp
+;;        "C-M-u" #'sp-backward-up-sexp
+;;        "C-M-d" #'sp-down-sexp
+;;        "C-M-p" #'sp-backward-down-sexp
+;;        "C-M-n" #'sp-up-sexp
+;;        "C-M-s" #'sp-splice-sexp
+;;        "C-)" #'sp-forward-slurp-sexp
+;;        "C-}" #'sp-forward-barf-sexp
+;;        "C-(" #'sp-backward-slurp-sexp
+;;        "C-M-)" #'sp-backward-slurp-sexp
+;;        "C-M-)" #'sp-backward-barf-sexp))
 
 ;;
 ;; Indentation
@@ -30,36 +62,9 @@
       doom-variable-pitch-font (font-spec :family "Fira Code")
       doom-unicode-font (font-spec :family "Fira Code" :size 12)
       doom-big-font (font-spec :family "Fira Code" :size 18))
+
 (add-hook! 'doom-load-theme-hook :append
            (doom/reload-font))
-
-
-(require 'json)
-
-(custom-theme-set-faces
- 'user
- '(org-block ((t (:inherit fixed-pitch))))
- '(org-code ((t (:inherit (shadow fixed-pitch)))))
- '(org-document-info ((t (:foreground "dark orange"))))
- '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
- '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
- '(org-link ((t (:foreground "royal blue" :underline t))))
- '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-property-value ((t (:inherit fixed-pitch))) t)
- '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
- '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
- '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
- '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
-
-
-;;
-;; Browser
-;;
-(setq browse-url-browser-function 'browse-url-generic)
-(when (executable-find "qutebrowser")
-  (setq browse-url-generic-program "qutebrowser"))
-(when (getenv "BROWSER")
-  (setq browse-url-generic-program (getenv "BROWSER")))
 
 ;;
 ;; Misc
@@ -83,31 +88,6 @@
 ;; Auto refresh buffers to what is changed on disk. This should help with syncthing and org agenda
 (global-auto-revert-mode t)
 
-;;
-;; For commit signing passphrase caching and finding other programs
-;;
-(require 'exec-path-from-shell)
-
-(with-eval-after-load 'exec-path-from-shell
-  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
-    (add-to-list 'exec-path-from-shell-variables var)))
-
-(when (or (memq window-system '(mac ns x pgtk))
-          (unless (memq system-type '(ms-dos windows-nt))
-            (daemonp)))
-  (exec-path-from-shell-initialize))
-
-(provide 'init-exec-path)
-
-
-;;
-;; Rust settings
-;;
-(after! rustic
-  (setq rustic-format-trigger 'on-compile))
-
-(add-hook 'rustic-mode-hook
-          (lambda () (setq indent-tabs-mode nil)))
 
 ;;
 ;; Magit
@@ -116,32 +96,6 @@
   (setq git-commit-summary-max-length 100)
 )
 
-;;;; For some rust projects, like the engine, compiling them with the default features will cause
-;;;; rust-analyzer to complain with many errors due to functions only compiled with the `test`
-;;;; feature flag. This causes a lot of noise to sift through to find real compile errors.
-;;;; This function allows these projects to be configured with lsp features such that these errors don't occur.
-;;(defun novcn/set-rust-project-features ()
-;;  "Set the configured lsp features for the current rust project."
-;;  (setq lsp-rust-features-before lsp-rust-features)
-;;  (defvar local-repo)
-;;  (setq local-repo (shell-command-to-string "basename $(git rev-parse --show-toplevel) | tr -d '\n'"))
-;;  (pcase local-repo
-;;    ("engine-rs" (setq lsp-rust-features ["test"]))
-;;    ;; Can configure any other repos that should use rust-analyzer with other than default features.
-;;    ;; default
-;;    (default (setq lsp-rust-features [])))
-;;  ;; Errors occur without a short sleep
-;;  (sleep-for 1)
-;;  ;; Check to see if we're in an enabled lsp workspace and if our features are different than before
-;;  (if (and (fboundp 'lsp-restart-workspace) (not (eq lsp-rust-features-before lsp-rust-features)))
-;;      (lsp-restart-workspace)))
-;;
-;;;; Hook this into every project switch
-;;(add-hook 'projectile-after-switch-project-hook 'novcn/set-rust-project-features)
-;;
-;;(add-hook! rustic-mode (set-fill-column 100))
-;;(add-hook 'rust-mode-hook (lambda ()
-;;                            (lsp-activate-if-already-activated 'rust-analyzer)))
 
 (setq treemacs-no-delete-other-windows nil)
 
@@ -167,9 +121,6 @@
 ;;
 ;; Parens
 ;;
-;;(after! smartparens
-;;  (smartparens-global-mode -1))
-
 (after! smartparens
     (let ((unless-list '(sp-point-before-word-p
                          sp-point-after-word-p
@@ -186,21 +137,10 @@
 ;; Org mode settings
 ;; This is why we are here. This is why you even write elisp.
 ;;
-(setq novcn/org-agenda-directory (file-truename "~/org"))
 
 ;; Save whatever's in the system clipboard before
 ;; replacing it with Emacs's text.
 (setq save-interprogram-paste-before-kill t)
-
-;; Custom faces for markdown headers
-(custom-set-faces!
-'(markdown-header-delimiter-face :foreground "#616161" :height 0.9)
-'(markdown-header-face-1 :height 1.8 :foreground "#A3BE8C" :weight extra-bold :inherit markdown-header-face)
-'(markdown-header-face-2 :height 1.4 :foreground "#EBCB8B" :weight extra-bold :inherit markdown-header-face)
-'(markdown-header-face-3 :height 1.2 :foreground "#D08770" :weight extra-bold :inherit markdown-header-face)
-'(markdown-header-face-4 :height 1.15 :foreground "#BF616A" :weight bold :inherit markdown-header-face)
-'(markdown-header-face-5 :height 1.1 :foreground "#b48ead" :weight bold :inherit markdown-header-face)
-'(markdown-header-face-6 :height 1.05 :foreground "#5e81ac" :weight semi-bold :inherit markdown-header-face))
 
 (after! org
   (customize-set-variable 'org-blank-before-new-entry'((heading . nil)(plain-list-item . nil)))(setq org-cycle-separator-lines 1)
@@ -217,17 +157,13 @@
 
   ;; Org General settings
   (setq org-directory "~/org/")
+  (setq novcn/org-agenda-directory (file-truename "~/org/gtd"))
   (setq org-ellipsis " ▼ ") ;; just to be fancy
 
   ;; These are to speed up agenda https://orgmode.org/manual/Speeding-Up-Your-Agendas.html
   (setq org-agenda-dim-blocked-tasks nil)
   (setq org-agenda-use-tag-inheritance nil)
   (setq org-agenda-ignore-properties '(effort appt stats category))
-
-;;
-;;
-
-      ;;'(())
 
   (setq org-archive-location "~/org/archive/%s::datetree/* Archived tasks")
 
@@ -263,10 +199,10 @@
         (0.0 . org-upcoming-distant-deadline)))
 
   (setq org-capture-templates
-      `(("i" "Inbox" entry  (file "inbox.org")
+      `(("i" "Inbox" entry  (file "gtd/inbox.org")
          ,(concat "* TODO %?\n"
                   "/Entered on/ %U"))
-        ("s" "Slipbox" entry  (file "braindump/org/inbox.org")
+        ("s" "Slipbox" entry  (file "gtd/braindump/inbox.org")
          "* %?\n")))
 
   (defun novcn/org-capture-inbox ()
@@ -305,11 +241,6 @@
                 ("REVIEW" :foreground "chartreuse3" :weight bold)
                 ("CANCELLED" :foreground "DeepSkyBlue4" :weight bold)))))
 
-(defun my/org-insert-heading-newline ()
-  "Add an extra newline before a new org heading."
-  (org-return))
-
-(advice-add 'org-insert-heading :after 'my/org-insert-heading-newline)
 ;;
 ;; Org agenda mode settings
 ;;
@@ -318,11 +249,10 @@
   (setq org-agenda-skip-scheduled-if-done t)
   (setq org-agenda-skip-deadline-if-done t)
 
-  (setq org-agenda-include-deadlines t)
+  (setq org-agenda-include-deadlines 'unencrypted)
   (setq org-agenda-block-separator nil)
   (setq org-agenda-tags-column 100)
   (setq org-agenda-compact-blocks t)
-  (org-super-agenda-mode)
 
   ;; save all org buffers - SPC m s
   (map!
@@ -338,48 +268,11 @@
          "/home/novcn/org/.undodir"
          ])
 
-  (add-hook 'org-agenda-mode-hook
-            (lambda ()
-              (setq org-agenda-files
-                    (seq-filter
-                     (lambda (path)
-                       (not (seq-some
-                             (lambda (x) (string-match x path))
-                             agendaSkipDirs )))
-                     (directory-files-recursively "/home/novcn/org/issues" "\.org$")))))
-
-  ;;
-  ;; Org super-agenda settings
-  ;;
-  (use-package! org-super-agenda
-    :commands (org-super-agenda-mode))
-
-  ;;
-  ;; Org super agenda custom
-  ;;
-  (setq org-agenda-custom-commands
-        '(("o" "Overview"
-           ((agenda "" ((org-agenda-span 'day)
-                        (org-super-agenda-groups
-                         '((:name "Today"
-                            :time-grid t
-                            :date today
-                            :todo "TODAY"
-                            :scheduled today
-                            :order 1))))))))))
-
-
-  (setq org-agenda-custom-commands
-      '(
-        (" " "Jira issues" agenda "Jira issues assigned to me"
-         ((org-agenda-files '("~/org/issues"))))
-        (" " "Inbox" agenda "To Refile"
-         ((org-agenda-files '("~/org/inbox.org"))))
-       )
-      )
-
+  (require 'find-lisp)
+  (setq novcn/org-agenda-directory
+        (expand-file-name "gtd/" org-directory))
+  (setq org-agenda-files
+        (directory-files-recursively novcn/org-agenda-directory "\.org$")))
 
 (setq projectile-project-search-path '("~/src/rust/" "~/src/node" "~/src/infra" "~/prj"))
-
 (add-hook 'sh-mode-hook (lambda () (sh-electric-here-document-mode -1)))
-
--- packages.el
+++ packages.el
@@ -1,19 +1,14 @@
-;; Souped up org agenda
-(package! org-super-agenda)
-
-;; Distraction free writing
-(package! olivetti)
-
-(package! org-modern)
-
-;; Required for catpuccin
-(package! autothemer)
-
-;; Kubernetes support
-(package! kubernetes)
-(package! kubernetes-evil)
-
-(package! catppuccin
-  :recipe (:host github :repo "catppuccin/emacs"))
-
-(package! exec-path-from-shell)
+;; -*- no-byte-compile: t; -*-
+(package! dired-narrow) ;; Directory shit
+(package! deadgrep) ;; rg in emacs
+(package! easy-kill) ;; easily kill stuff (as in cut/paste)
+(package! org-clock-convenience) ;; improve clock workflo
+(package! company-posframe) ;; candidate menu changes
+(package! org-roam ;; brain
+  :recipe (:host github :repo "org-roam/org-roam" :branch "main"))
+(package! modus-themes)
+(package! dash) ;; list library
+(package! org-roam-ui)
+(package! tree-sitter)
+(package! tree-sitter-langs)
+(package! jq-mode)
